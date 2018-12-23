22adb2db48f94a5adefab97d310b1b11bbb9375e
Joel Mueller <joel.mueller@gmail.com>
2018-12-22 20:03:42 -0600

    Bumping up AddRange size

--- Core.Collections.Benchmarks/List.AddRange.cs
+++ Core.Collections.Benchmarks/List.AddRange.cs
@@ -30,7 +30,7 @@ namespace Core.Collections.Benchmarks
 
         private PooledList<int> list;
 
-        [Params(1000, 10000, 100000)]
+        [Params(1000, 10000, 100000, 1000000)]
         public int N;
 
         [GlobalSetup]
